9b385fe232dfaf54309deaf2b71099693a8df869
ifiix <43194303+ifiix@users.noreply.github.com>
2018-09-17 11:10:37 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,62 +1,37 @@
-<!DOCTYPE html>
+<?php
+session_start();
+$message="";
+if(count($_POST)>0) {
+$con = mysqli_connect('127.0.0.1:3306','root','','admin') or die('Unable To connect');
+$result = mysqli_query($con,"SELECT * FROM login_user WHERE name='" . $_POST["name"] . "' and password = '". $_POST["password"]."'");
+$row = mysqli_fetch_array($result);
+if(is_array($row)) {
+$_SESSION["id"] = $row[id];
+$_SESSION["name"] = $row[name];
+} else {
+$message = "Invalid Username or Password!";
+}
+}
+if(isset($_SESSION["id"])) {
+header("Location:index.php");
+}
+?>
 <html>
 <head>
-    <title></title>
+<title>User Login</title>
 </head>
 <body>
-    <table align="center" bgcolor="#CCCCCC" border="0" cellpadding="0"
-    cellspacing="1" width="300">
-        <tr>
-            <td>
-                <form method="post" name="">
-                    <table bgcolor="#FFFFFF" border="0" cellpadding="3"
-                    cellspacing="1" width="100%">
-                        <tr>
-                            <td align="center" colspan="3"><strong>User
-                            Login</strong></td>
-                        </tr>
-                        <tr>
-                            <td width="78">Username</td>
-                            <td width="6">:</td>
-                            <td width="294"><input id="username" name=
-                            "username" type="text"></td>
-                        </tr>
-                        <tr>
-                            <td>Password</td>
-                            <td>:</td>
-                            <td><input id="password" name="password" type=
-                            "password"></td>
-                        </tr>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td>&nbsp;</td>
-                            <td><input name="submit" type="submit" value=
-                            "Login"> <input name="reset" type="reset" value=
-                            "reset"></td>
-                        </tr>
-                    </table>
-                </form>
-            </td>
-        </tr>
-    </table>
-    <?php
-    if (isset($_POST['submit']))
-        {     
-    include("config.php");
-    session_start();
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-    $_SESSION['login_user']=$username; 
-    $query = mysql_query("SELECT username FROM login WHERE username='$username' and password='$password'");
-     if (mysql_num_rows($query) != 0)
-    {
-     echo "<script language='javascript' type='text/javascript'> location.href='home.php' </script>";   
-      }
-      else
-      {
-    echo "<script type='text/javascript'>alert('User Name Or Password Invalid!')</script>";
-    }
-    }
-    ?>
+<form name="frmUser" method="post" action="" align="center">
+<div class="message"><?php if($message!="") { echo $message; } ?></div>
+<h3 align="center">Enter Login Details</h3>
+Username:<br>
+<input type="text" name="name">
+<br>
+Password:<br>
+<input type="password" name="password">
+<br><br>
+<input type="submit" name="submit" value="Submit">
+<input type="reset">
+</form>
 </body>
 </html>
